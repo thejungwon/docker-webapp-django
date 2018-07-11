5c06680cdb206d673b6ff01f72635e61b9774d94
Jungwon Seo <wsbtgt@gmail.com>
2018-07-11 09:33:44 +0200

    ggg

--- src/radixProject/settings.py
+++ src/radixProject/settings.py
@@ -73,14 +73,20 @@ WSGI_APPLICATION = 'radixProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': 'postgres',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'postgres',
-        'PORT': 5432,
     }
 }
 
