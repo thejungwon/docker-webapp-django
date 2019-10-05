c8742909bb60e5c6658832167f2bed6c60f0851a
Bakos Martin <bakosmartin@gmail.com>
2019-10-05 16:26:26 +0200

    src: added sign up page to create new users

--- src/radixProject/urls.py
+++ src/radixProject/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
