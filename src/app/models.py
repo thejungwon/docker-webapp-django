cb955de0141f293792f7b4c24dd5ded38593cb2f
Attila Tóth <muchcoffe@gmail.com>
2019-10-06 22:18:05 +0200

    added models for pizza

--- src/app/models.py
+++ src/app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
 class Post(models.Model):
     date = models.DateTimeField('date published')
     photo = models.TextField()
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
