5c9f74cda6751591128993fc397d2d76d373fb40
Botond Mate Geri <boti.geri96@gmail.com>
2019-10-08 19:57:39 +0200

    Create new file for navigation bar

--- src/app/urls.py
+++ src/app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     path('pizzas/', views.pizzas, name='pizzas'),
-    path('signup/', views.SignUp.as_view(), name='signup')
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('account/profile/', views.profile, name='profile'),
+    path('pizzalist/', views.pizzalist, name='pizzalist'),
+    path('account/myorders/', views.myorders, name='myorders')
 ]
