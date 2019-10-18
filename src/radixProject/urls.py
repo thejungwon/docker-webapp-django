0db5e92c30dc19202f08a2201588795587b2f631
Botond Mate Geri <boti.geri96@gmail.com>
2019-10-18 19:28:00 +0200

    Delete dead code.

--- src/radixProject/urls.py
+++ src/radixProject/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('app.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('app.urls')),
     path('', include('django.contrib.auth.urls')),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
