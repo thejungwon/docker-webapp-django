66503a3264077cc754c580e74ea75dac4e72a275
Attila Tóth <attila.toth@syrg.app>
2019-10-23 22:35:35 +0200

    removed migrations and removed an unused import

--- src/radixProject/urls.py
+++ src/radixProject/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
-from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
