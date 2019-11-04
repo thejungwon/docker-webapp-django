c8c7fc2f519533b30ee06ac94f786f25f23a2bc1
Borsos Levente <borsos.levente@navicon.hu>
2019-11-04 21:14:02 +0100

    Fixed order and transaction double relationship

--- src/app/domain/models.py
+++ src/app/domain/models.py
@@ -74,7 +74,7 @@ class PriviligeLevel(models.Model):
 
 class Transaction(models.Model):
     #  Relationships
-    O_T_O_Order_Transaction = models.OneToOneField("app.Order", on_delete=models.CASCADE, null = True)
+    O_T_O_Order_Transaction = models.OneToOneField("app.Order", on_delete=models.CASCADE)
 
     #  Fields
     payment_type = models.TextField(max_length=50)
@@ -202,7 +202,6 @@ class Courier(models.Model):
 
 class Order(models.Model):
     #  Relationships
-    O_T_O_Transaction_Order = models.OneToOneField(Transaction, on_delete=models.CASCADE)
     Courier_Orders = models.ForeignKey(Courier, on_delete=models.CASCADE)
     O_T_M_User_Orders = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     O_T_O_Address_Order = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
